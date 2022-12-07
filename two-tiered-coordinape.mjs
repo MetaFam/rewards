a82611123c756dc978f33124d0a6a3adc7559f8a
Scott Stevenson <scott@1box.onmicrosoft.com>
2022-12-07 14:56:02 -0800

    calculations for Seed distribution

--- two-tiered-coordinape.mjs
+++ two-tiered-coordinape.mjs
@@ -1,24 +1,21 @@
-// import {
-//   ApolloClient,
-//   InMemoryCache,
-//   // ApolloProvider,
-//   // useQuery,
-//   // gql
-// } from "@apollo/client"
+#! /usr/bin/env node
+
 import fetch from 'node-fetch'
+import yargs from 'yargs'
+
 import apollo from '@apollo/client'
 import 'dotenv/config'
 const { ApolloClient, InMemoryCache, HttpLink, gql } = apollo
-// const { ApolloClient, InMemoryCache } = require('@apollo/client')
 
 const query = gql`
   query MyCircles($name: String!) {
     circles(limit: 3, where: {name: {_eq: $name}}) {
+      name
       users(order_by: {created_at: asc}) {
-        id
         name
         give_token_received
-        give_token_remaining
+        non_receiver
+        address
       }
     }
   }
@@ -35,46 +32,111 @@ const client = new ApolloClient({
   })
 })
 
-const main = async () => {
+
+const args = (
+  yargs(process.argv.slice(2))
+  .options({
+    circle: {
+      type: 'string',
+      alias: 'c',
+      description: 'Name of the top level Circle',
+      demandOption: true,
+    },
+    seeds: {
+      type: 'number',
+      alias: 's',
+      description: 'Number of SEEDs to distribute',
+      demandOption: true,
+    },
+  })
+  .alias('h', 'help')
+  .help()
+  // .showHelpOnFail(true, 'HELP!')
+)
+const argv = await args.argv
+
+const getCircle = async (name) => {
   const result = await client.query({
     query,
-    variables: { name: 'MetaGame Guilds & Raids' },
+    variables: { name },
   })
-  // console.log(JSON.stringify(result, null, 2))
-  const groups = result.data.circles[0].users.filter((user) => (
-    /(guild|raid)/i.test(user.name)
-  ))
-  const total = (
-    groups.map((group) => group.give_token_received)
-      .reduce((acc, amt) => acc + amt, 0)
-  )
-  await Promise.all(groups.map(async (group) => {
-    const circle = await client.query({
-      query,
-      variables: { name: group.name },
-    })
-    console.log(
-      `${group.name}: ${(group.give_token_received * 100 / total).toFixed(2)}%`
-    )
-    const { users } = circle.data.circles[0] ?? {}
-    if (users) {
-      const userTotal = (
-        users.map((user) => user.give_token_received)
-          .reduce((acc, amt) => acc + amt, 0)
-      )
-      users.forEach((user) => {
-        if (userTotal === 0) {
-          console.log(`  ${user.name}: 0%`)
-        } else {
-          console.log(
-            `  ${user.name}: ${(user.give_token_received * 100 / userTotal).toFixed(2)}%`
-          )
-        }
-      })
-    }
-  }))
+  const [circle] = result.data.circles
+  return circle
 }
 
+const totalReceived = (users) => {
+  const total = (
+    users.map((user) => user.give_token_received)
+      .reduce((acc, amt) => acc + amt, 0)
+  )
+
+  if(total === 0) throw new Error('no GIVE allocated')
+  return total
+}
+const name = argv.circle
+const main = async () => {
+  const top = await getCircle(name) 
+  
+  if (!top) new Error(`No circle named: "${name}"`)
+
+  // console.log(JSON.stringify(result, null, 2))
+  
+  const groups = top.users.filter((user) => (
+    (!user.non_receiver)
+  ))
+
+  const total = totalReceived(groups)
+
+  const dist = await Promise.all(groups.map(async (group) => {
+    try {
+      const circle = await getCircle(group.name)
+      const groupPct = group.give_token_received / total 
+      process.stderr.write(
+        `${group.name}: ${(groupPct * 100).toFixed(2)}%:`
+        + ` ${argv.seeds * groupPct}\n`
+      )
+      if(circle){
+        const { users } = circle
+        const userTotal = totalReceived(users)
+        return users.map((user) => {
+          const received = user.give_token_received
+          const userPct = received / userTotal
+          process.stderr.write(
+            ` ${user.name}: (${user.address})`
+            + ` ${(userPct * 100).toFixed(2)}%:`
+            + ` ${argv.seeds * groupPct * userPct}\n`
+          )
+          return {
+            name: user.name,
+            address: user.address,
+            amount: argv.seeds * groupPct * userPct,
+          }
+        })
+      } else {
+        return {
+          name: group.name,
+          address: group.address,
+          amount: argv.seeds * groupPct,
+        }
+      }
+    } catch (error){
+      console.error(error.message)
+    }
+  }))
+  const entries = dist.flat()
+  const recipients = new Set(entries.map(({address}) => address))
+  recipients.forEach((address) => {
+    const userEntries = entries.filter(({address:addr}) => (
+      addr === address
+    ))
+    const total = (
+      userEntries.map(({amount}) => amount)
+      .reduce((acc, amt) => acc + amt, 0)
+    )
+    const [user] = userEntries
+    console.log(`${user.name},${user.address},${total}`) 
+  }
+)}
 
 main().then(
   () => { process.exit(0) }
